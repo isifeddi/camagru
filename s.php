8a8026cb8dfd35f72ca6603bb87e03fb2738bee3
Ilyas Sifeddine <isifeddi@e2r2p8.1337.ma>
2019-04-28 22:49:23 +0100

    f

--- s.php
+++ s.php
@@ -1,27 +1,30 @@
 <?php
-
-try{
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=camagru", "root", "tiger");
-    echo "connection successful";
-}catch(PDOException $e){
-    echo "connection failed" . $e->getMessage();
+if(isset($_POST['submit']))
+{
+    try{
+    
+        $pdoConnect = new PDO ("mysql:host=127.0.0.1;dbname=camagru","root","tiger");
+        //
+            $firstname = $_POST['firstname'];
+            $lastname =  $_POST['lastname'];
+            $birthdate = $_POST['birthdate'];
+            $gender = $_POST['gender'];
+            $email =  $_POST['email'];
+            $username =  $_POST['username'];
+            $password1 =  $_POST['password1'];
+            $password2 =  $_POST['password2'];
+        //
+        $pdoQuery = $sql = "INSERT INTO `utils`(`id`, `firstname`, `lastname`, `birthdate`, `gender`, `email`, `username`, `password`) VALUES (:firstname ,:lastname, :birthdate, :gender, :email, :username, :password)";
+        $pdoResult = $pdoConnect->prepare($pdoQuery);
+        $pdoExec = $pdoResult->execute(array(":firstname"=>$firstname, ":lastname"=>$lastname, ":birthdate"=>$birthdate, ":gender"=>$gender, ":email"=>$email, ":username"=>$username,  ":password"=>$password1));
+        if($pdoExec){
+            echo "data insert";
+        }
+        
+    
+    }
+    catch(PDOException $e){
+        echo "error".$e->getMessage();
+    }
 }
-
-$status = "";
-
-    $firstname = $_POST['firstname'];
-    $lastname =  $_POST['lastname'];
-    $birthdate = $_POST['birthdate'];
-    $gender = $_POST['gender'];
-    $email =  $_POST['email'];
-    $username =  $_POST['username'];
-    $password =  $_POST['password'];
-    //$password2 =  $_POST['password2'];
-
-    $sql ="INSERT INTO `users`(`id`, `firstname`, `lastname`, `birthdate`, `gender`, `email`, `username`, `password`) VALUES (:$firstname ,:$lastname, :$birthdate, :$gender, :$email, :$username, :$password)";
-    $stmt = $pdo->prepare($sql);
-
-    $stmt->execute(array(":firstname" => $firstname, ":lastname" => $lastname, ":birthdate" => $birthdate, ":gender" => $gender, ":email" => $email, ":username" => $username,  ":password" => $password));
-     if($test)
-        echo "khdama";
 ?>
